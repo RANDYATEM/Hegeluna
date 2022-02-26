8d017c7fdb7ba3deebe5039323957543104df032
ATEM <evitaatem@gmail.com>
2022-02-26 18:23:13 +0000

    First commit

---
+++ developerslist.sh
@@ -0,0 +1,9 @@
+#list of developers working on this project
+Randy 
+Paul
+Eliane
+samuel
+sam
+cyril
+clinton
+moliki
---
+++ login.sh
@@ -0,0 +1,7 @@
+#login script
+echo " Welcome to canada's fastest growing clothing brand, Please enter your name"
+read name
+echo "$name please enter your password"
+read -s password
+echo "You have successfuly logged into your Hegeluna account"
+echo "Today's date is: `date`"
